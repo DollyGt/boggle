c69a284c29df042903fa724a0cff210f1ccad9dc
DollyG <dollsgoitse@yahoo.com>
2018-05-22 16:06:14 +0000

    first commit

---
+++ comp.py
@@ -0,0 +1,19 @@
+# example of list comprehension/ python comprehension challenges
+# l = [1,3,5]
+
+# p = []
+# for n in l:
+#     p.append(n*2)
+#print([n*2 for n in l])
+    
+# print(p)
+
+# l = [1,3,5]
+
+# p = [n*2 for n in l]
+
+# print(p)
+
+# l = [0,2,4,6,8,10,12]
+print([i*2 for i in range(7)])
+
---
+++ recursion.py
@@ -0,0 +1,30 @@
+
+# def sumlist(l):
+#     if l == []:
+#         return 0
+#     else:
+#         first = l[0]
+#         rest = l[1:]
+#         return first + sumlist(rest)
+
+    
+# print(sumlist([1, 4, 13, 34, 56]))
+
+# def getSum(piece):
+#     if len(piece)==0:
+#         return 0
+#     else:
+#         return piece[0] + getSum(piece[1:]) 
+# print getSum([1, 3, 4, 2, 5])
+
+def maxlist(l):
+    if len(l) == 1:
+        return l[0] 
+    else:
+        m = l[1:]
+        return m if m > l[0] else l[0]
+
+print(maxlist([2,13,5,7,8]))
+
+        
+        
